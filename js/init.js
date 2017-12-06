45043076419ea2dffa992f67ab25d0473f2b2d07
xlopn <flat.pfw37gins.tab@gmail.com>
2017-12-06 20:14:33 +0900

    add fixed

--- js/init.js
+++
@@ -1,8 +0,0 @@
-(function($){
-  $(function(){
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $(".dropdown-button").dropdown();
-    $('.slider').slider({full_width: true});
-  }); 
-})(jQuery);
