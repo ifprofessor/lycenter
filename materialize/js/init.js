9a9ec2fa583c66e1772e6b12f25ba0cfd12c1151
xlopn <flat.pfw37gins.tab@gmail.com>
2017-12-06 19:37:49 +0900

    add fixed

---
+++ materialize/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $(".dropdown-button").dropdown();
+    $('.slider').slider({full_width: true});
+  }); 
+})(jQuery);
